7fd59a75bb37b965c7e96f87c203523b296cef80
Alexis Campusano <ale.cmpb@gmail.com>
2023-07-07 21:25:38 -0400

    feat: add functionality to create capacitation

--- src/main/java/cl/awakelab/springmvc/controllers/CreateCapacitationController.java
+++ src/main/java/cl/awakelab/springmvc/controllers/CreateCapacitationController.java
@@ -1,16 +1,28 @@
 package cl.awakelab.springmvc.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import cl.awakelab.springmvc.dao.ICapacitationDao;
+import cl.awakelab.springmvc.models.Capacitation;
+
 @Controller
 public class CreateCapacitationController {
 
-    @RequestMapping(path = "/create-capacitation", method = RequestMethod.GET)
-    public ModelAndView createCapacitation() {
-        return new ModelAndView("create-capacitation");
-    }
+	@Autowired
+	private ICapacitationDao capacitacionDao;
 
+	@RequestMapping(path = "/create-capacitation", method = RequestMethod.GET)
+	public ModelAndView createCapacitation() {
+		return new ModelAndView("create-capacitation");
+	}
+
+	@RequestMapping(path = "/create-capacitation", method = RequestMethod.POST)
+	public void submitCapacitation(Capacitation capacitacion) {
+		capacitacionDao.createCapacitation(capacitacion);
+
+	}
 }
